44b567fec729392c5d0f0f6d4be930a96981c503
Daniel German <danielchuksgerman@gmail.com>
2024-05-02 10:32:23 +0000

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Takes two strings as arguments and returns a concatenated string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Function that takes string str1 and a string str2 as arguments and
+    returns a concatenated string."""
+    return str1 + str2
