7f1d8f546e7dc809cd66edb9e34a04cc3127285e
Daniel German <danielchuksgerman@gmail.com>
2024-05-02 10:51:17 +0000

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Takes a float and returns the string representation"""
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of float n."""
+    return str(n)
