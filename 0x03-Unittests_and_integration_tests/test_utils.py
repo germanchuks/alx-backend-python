6bbececb5d081f3cab49b1217be37be5bb7c3d23
Daniel German <danielchuksgerman@gmail.com>
2024-05-23 13:38:17 +0000

    Parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,7 @@
 """Module for Unittests and Integration Tests"""
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from unittest.mock import patch
 
 
@@ -50,3 +50,29 @@ class TestGetJson(unittest.TestCase):
         self.assertEqual(get_json(test_url), test_payload)
         mock.assert_called_once()
         patcher.stop()
+
+
+class TestMemoize(unittest.TestCase):
+    """Unittests for utils.memoize."""
+
+    def test_memoize(self):
+        """
+        Test that when calling a_property twice, the correct result is
+        returned but a_method is only called once using assert_called_once.
+        """
+
+        class TestClass:
+            """Wrapper class for testing the memoize decorator."""
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock:
+            test_class = TestClass()
+            test_class.a_property()
+            test_class.a_property()
+            mock.assert_called_once()
